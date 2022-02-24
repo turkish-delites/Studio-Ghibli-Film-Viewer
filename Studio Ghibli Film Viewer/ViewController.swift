855c937bd897d68f86037b44cb1225754e82b95d
turkish-delites <bakkal27@gmail.com>
2022-02-24 14:17:51 -0600

    Initial Commit

---
+++ Studio Ghibli Film Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studio Ghibli Film Viewer
+//
+//  Created by Evan Bakkal on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
