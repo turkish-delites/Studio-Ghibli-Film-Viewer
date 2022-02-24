256ab96339629ad36b0977be177ce04354535bd4
turkish-delites <bakkal27@gmail.com>
2022-02-24 14:55:29 -0600

    Added Navigation Controller, View Controllers, Image view and labels in Interface Builder. Also added new MovieViewController

---
+++ Studio Ghibli Film Viewer/MovieViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieViewController.swift
+//  Studio Ghibli Film Viewer
+//
+//  Created by Evan Bakkal on 2/24/22.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
